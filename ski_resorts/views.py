54d919d7cd3145c2d975d6cf122eae810508917a
bdavis83 <bdavis83@gmail.com>
2023-07-26 10:18:25 -0400

    get all skiresorts endpoint now working

--- ski_resorts/views.py
+++ ski_resorts/views.py
@@ -1,22 +1,27 @@
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
 from .models import SkiResort
 from .serializer import SkiResortSerializer
-from django.shortcuts import get_object_or_404
 
-@api_view (['GET'])
+@api_view(['GET', 'POST'])  # Allow both GET and POST requests for this view
 @permission_classes([AllowAny])
+def ski_resorts_view(request):
+    if request.method == 'GET':
+        return get_all_ski_resorts(request)
+    elif request.method == 'POST':
+        return add_ski_resort(request)
+
+@api_view(['GET'])
 def get_all_ski_resorts(request):
     ski_resorts = SkiResort.objects.all()
     serializer = SkiResortSerializer(ski_resorts, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-@permission_classes([AllowAny])
-def add_ski_resort (request):
-    if request.method=='POST':
+def add_ski_resort(request):
+    if request.method == 'POST':
         serializer = SkiResortSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
