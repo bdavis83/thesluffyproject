32fc509d17766fb77b840574d36dce4ce4c76d0a
bdavis83 <bdavis83@gmail.com>
2023-07-26 10:17:54 -0400

    fixed views and skir_reosrts.urls.py

--- ski_resorts/urls.py
+++ ski_resorts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.get_all_ski_resorts),
+    path('', views.ski_resorts_view),
+    path('all/', views.get_all_ski_resorts),
     path('add/', views.add_ski_resort),
 ]
