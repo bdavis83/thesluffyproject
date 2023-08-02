dd7413e2e86173dfa589d7a587b2da31e75c0bab
bdavis83 <bdavis83@gmail.com>
2023-08-02 17:28:19 -0400

    resintalled react + got LeafletMaps funcitoning

---
+++ Frontend/sluffy-react-app/src/App.js
@@ -0,0 +1,12 @@
+import LeafletMap from "./components/LeafletMap";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <LeafletMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/sluffy-react-app/src/components/LeafletMap.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker, Popup } from "react-leaflet";
+import { useMap } from "react-leaflet/hooks";
+import "leaflet/dist/leaflet.css";
+
+export default function LeafletMap() {
+  return (
+    <MapContainer center={[48.3689, -99.9962]} zoom={4} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
