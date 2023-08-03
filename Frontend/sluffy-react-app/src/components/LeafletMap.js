0ed45619d4868de738fab58214d5f39c1a953bde
bdavis83 <bdavis83@gmail.com>
2023-08-03 13:34:08 -0400

    fixed local_settings.py + leaflet map displays

--- Frontend/sluffy-react-app/src/components/LeafletMap.js
+++ Frontend/sluffy-react-app/src/components/LeafletMap.js
@@ -1,22 +1,39 @@
-import React from "react";
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { Marker, Popup } from "react-leaflet";
-import { useMap } from "react-leaflet/hooks";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
 
-export default function LeafletMap() {
+const LeafletMap = () => {
+  const [skiResort, setSkiResort] = useState([]);
+
+  async function fetchSkiResorts() {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/ski_resorts/all/"
+      );
+      setSkiResort(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSkiResorts();
+  }, []);
+
   return (
     <MapContainer center={[48.3689, -99.9962]} zoom={4} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {skiResort.map((resort) => (
+        <Marker key={resort.id} position={[resort.latitude, resort.longitude]}>
+          <Popup>{resort.title}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
-}
+};
+
+export default LeafletMap;
